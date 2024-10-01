d9747d2f3886f719efc7bdf71eb0fb0a197f5c9a
marcopeocchi <marco.piovanello@gmail.com>
2024-10-01 20:29:22 +0200

    changed buffer size

--- internal/common.go
+++ internal/common.go
@@ -1,7 +1,7 @@
 package internal
 
 const (
-	CHUNK_SIZE      int32 = 512 * 1000
+	CHUNK_SIZE      int32 = 1024 * 1000
 	DELIMITER_SEQ   byte  = '\n'
 	PUBLIC_KEY_SIZE       = 32 // X25519 -> 32 | P256 -> (32*2)+1
 )
